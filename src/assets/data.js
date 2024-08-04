7fb27a6a708590b332c06f197c845b5b692b55d1
hajarzain <hajarzain222@gmail.com>
2024-08-05 02:37:34 +0300

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "A force is a ...... or a ....... that causes movement.",
+        option1: "a. push , taste",
+        option2: "b. push , pull",
+        option3: "c. pull , run",
+        option4: "d. pull , lift",
+        answer: 2,
+    },
+    {
+        question: "What is the force that pulls objects to the center of Earth called ......",
+        option1: "a. motion.",
+        option2: "b. friction.",
+        option3: "c. gravity.",
+        option4: "d. force.",
+        answer: 3,
+    },
+    {
+        question: "When an ice cube melts, it changes from ......",
+        option1: "a. a solid to a liquid.",
+        option2: "b. a liquid to a gas.",
+        option3: "c. a solid to a gas.",
+        option4: "d. a gas to a solid.",
+        answer: 1,
+    },
+    {
+        question: "Most magnets are made of ......",
+        option1: "a. iron or paper.",
+        option2: "b. paper or steel.",
+        option3: "c. steel or rock.",
+        option4: "d. iron or steel.",
+        answer: 4,
+    },
+    {
+        question: "A baseball lies on the floor. On what instance it will move ......",
+        option1: "a. When force acts on it.",
+        option2: "b. When it was pulled by gravity.",
+        option3: "c. When the area is dark.",
+        option4: "d. It will not move at all.",
+        answer: 1,
+    },
+]
